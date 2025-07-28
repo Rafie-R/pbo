12a2ebff15fd96ebf7b05bc9fbd1053dfa0d6a3d
Rafie-R <Rafieashraf716@gmail.com>
2025-07-28 14:58:25 +0700

    Add files via upload

---
+++ JsCoding.js
@@ -0,0 +1,83 @@
+/* function sapa() {
+  console.log(
+    "Sapa is a beautiful place in Vietnam, known for its terraced rice fields and stunning mountain scenery."
+  );
+}
+
+sapa();
+
+const row = 50; // Eror
+let nilai = 50;
+nilai = 100;
+
+if (nilai >= 75) {
+  console.log("Lulus");
+} else {
+  console.log("Tidak Lulus");
+}
+
+for (let i = 0; i < 5; i++) {
+  console.log("Perulangan ke-" + i);
+} 
+
+
+  // --Sedang Belajar--
+function sapa(nama) {
+  console.log("Halo, " + nama + "!");
+}
+
+sapa("Joy");
+sapa("Seulgi");
+
+function tambah(a, b) {
+  return a + b;
+}
+
+let hasil = tambah(5, 3);
+console.log("Hasil penjumlahan: " + hasil);
+
+
+// --Belajar Objek Properties
+let car = {
+  name: "Fiat",
+  model: 500,
+  weight: "850kg",
+  color: "white",
+
+  start: function () {
+    console.log("Mobil dinyalakan.");
+  },
+  drive: function () {
+    console.log("Mobil berjalan.");
+  },
+  bake: function () {
+    console.log("Mobil mengerem.");
+  },
+  stop: function () {
+    console.log("Mobil berhenti.");
+  },
+};
+
+console.log(car.name); // Output propeti
+car.start(); // Menjalankan method
+*/
+
+let BIRN = {
+  name: "Name: " + "Polygon",
+  gear: "Gear: " + 7,
+  color: "Color: " + "Merah",
+
+  start: function () {
+    console.log("Mobil mulai dikayuh.");
+  },
+
+  brake: function () {
+    console.log("Mobil mengerem.");
+  },
+};
+
+console.log(BIRN.name); // Output properti
+console.log(BIRN.gear); // Output properti
+console.log(BIRN.color); // Output properti
+BIRN.start(); // Menjalankan method
+BIRN.brake(); // Menjalankan method
